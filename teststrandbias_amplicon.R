655253d7fe9d725b2a0c4762f8eb15934912daa8
Duane Hassane <dhassane@med.cornell.edu>
2015-04-30 19:48:59 -0400

    Create teststrandbias_amplicon.R
    Adding col.names argument to read.table to circumvent tryCatch seems to have broken testing strand bias for amplicon mode VarDict output, which throws the "more columns than column names" error with amplicon data.  Creating a separate teststrandbias_amplicon.R script for amplicon mode is one solution.

---
+++ teststrandbias_amplicon.R
@@ -0,0 +1,22 @@
+#!/usr/bin/env Rscript
+
+#args <- commandArgs(trailingOnly = TRUE)
+
+#breaks amplicon calling
+#d <- read.table( file('stdin'), sep = "\t", header = F, colClasses=c("character", NA, NA, NA, NA, "character", "character", NA, NA, NA, NA, NA, NA, "character", NA, NA, NA, NA, NA, NA, NA, NA), col.names=c(1:34))
+
+colClasses=c("character","character",NA,NA,NA,"character","character",NA,NA,NA,NA,NA,NA,"character",NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,"character","character","character","character",NA,NA,NA,NA)
+d <- read.table( "vardict.test.out",sep = "\t", header = F,colClasses=colClasses,col.names=c(1:38))
+
+if (nrow(d) > 0){
+    pvalues <- vector(mode="double", length=dim(d)[1])
+    oddratio <- vector(mode="double", length=dim(d)[1])
+
+    for( i in 1:dim(d)[1] ) {
+        h <- fisher.test(matrix(c(d[i,10], d[i,11], d[i,12], d[i,13]), nrow=2))
+        pvalues[i] <- round(h$p.value, 5)
+        oddratio[i] <- round(h$estimate, 5)
+    }
+    write.table(data.frame(d[,1:20], pvalues, oddratio, d[,21:dim(d)[2]]), file = "", quote = F, sep = "\t", eol = "\n", row.names=F, col.names=F)
+}
+
