70e7891ed3dedfcbc5766806483fa44082e9a04a
vladsaveliev <vladsaveliev@me.com>
2015-06-04 00:26:24 +0300

    vardict2mut.pl: commandline-parametrizable paths

--- vardict2mut.pl
+++ vardict2mut.pl
@@ -1,25 +1,51 @@
 #!/usr/bin/perl -w
 
-use Getopt::Std;
+#use Getopt::Std;
+use Getopt::Long qw(:config no_ignore_case);
 use strict;
 
 our ($opt_n, $opt_f, $opt_F, $opt_H, $opt_D, $opt_V, $opt_M, $opt_R);
 
-getopts( 'HMn:f:F:D:V:R:' ) || USAGE();
+my $ruledir = "/users/kdld047/work/NGS/Wee1";
+my $filter_common_snp = "/group/cancer_informatics/tools_resources/NGS/genomes/hg19/Annotation/filter_common_snp.txt";
+my $snpeffect_export_polymorphic = "/group/cancer_informatics/tools_resources/NGS/genomes/hg19/Annotation/snpeffect_export_Polymorphic.txt";
+my $filter_common_artifacts = "/group/cancer_informatics/tools_resources/NGS/genomes/hg19/Annotation/filter_common_artifacts.txt";
+my $actionable_hotspot = "/group/cancer_informatics/tools_resources/NGS/genomes/hg19/Annotation/actionable_hotspot.txt";
+my $actionable = "/group/cancer_informatics/tools_resources/NGS/genomes/hg19/Annotation/actionable.txt";
+my $compendia_ms7_hotspot = "/group/cancer_informatics/tools_resources/NGS/genomes/hg19/Annotation/Compendia.MS7.Hotspot.txt";
+
+GetOptions ("n=s" => \$opt_n,
+            "f=f" => \$opt_f,
+            "F=f" => \$opt_F,
+            "H|?" => \$opt_H,
+            "D=i" => \$opt_D,
+            "V=i" => \$opt_V,
+            "M"   => \$opt_M,
+            "R=f" => \$opt_R,
+            "ruledir=s" => \$ruledir,
+            "filter_common_snp=s" => \$filter_common_snp,
+            "snpeffect_export_polymorphic=s" => \$snpeffect_export_polymorphic,
+            "filter_common_artifacts=s" => \$filter_common_artifacts,
+            "actionable_hotspot=s" => \$actionable_hotspot,
+            "actionable=s" => \$actionable,
+            "compendia_ms7_hotspot=s" => \$compendia_ms7_hotspot) or USAGE();
+
+#getopts( 'HMn:f:F:D:V:R:' ) || USAGE();
 $opt_H && USAGE();
-my $RULEDIR = "/users/kdld047/work/NGS/Wee1";
-my $tp53group1 = parseMut_tp53( "$RULEDIR/Rules/DNE.txt" );
-my $tp53group2 = parseMut_tp53( "$RULEDIR/Rules/TA0-25.txt" );
-my $tp53group3 = parseMut_tp53( "$RULEDIR/Rules/TA25-50_SOM_10x.txt" );
+
+my $tp53group1 = parseMut_tp53( "$ruledir/Rules/DNE.txt" );
+my $tp53group2 = parseMut_tp53( "$ruledir/Rules/TA0-25.txt" );
+my $tp53group3 = parseMut_tp53( "$ruledir/Rules/TA25-50_SOM_10x.txt" );
 my %tp53critical_aa_pos;
 my @tp53hg19splice = qw(7574034 7574035 7576851 7576852 7576927 7576928 7577017 7577018 7577156 7577157 7577497 7577498 7577609 7577610 7578175 7578176 7578290 7578291 7578369 7578370 7578555 7578556 7579310 7579311 7579591 7579592 7579698 7579699 7579722 7579723 7579837 7579838 7574033 7576926 7577155 7577608 7578289 7578554 7579590 7579721 7576853 7577019 7577499 7578177 7578371 7579312 7579700 7579839);
 my %tp53hg19splice = map { ($_, 1); } @tp53hg19splice;
+
 # Set up common SNP filter
 my $MINAF = $opt_f ? $opt_f : 0.05;
 my $MAXRATE = $opt_R ? $opt_R : 1.00;
 my $ACTMINAF = $opt_F ? $opt_F : ($MINAF/2 < 0.01 ? $MINAF/2 : 0.01);
 my %filter_snp;
-open( FSNP, "/group/cancer_informatics/tools_resources/NGS/genomes/hg19/Annotation/filter_common_snp.txt" );
+open( FSNP, $filter_common_snp );
 while( <FSNP> ) {
     chomp;
     my @a = split(/\t/);
@@ -29,7 +55,7 @@ while( <FSNP> ) {
 close( FSNP );
 
 my %snpeff_snp;
-open( SESNP, "/group/cancer_informatics/tools_resources/NGS/genomes/hg19/Annotation/snpeffect_export_Polymorphic.txt" );
+open( SESNP, $snpeffect_export_polymorphic );
 while( <SESNP> ) {
     chomp;
     my @a = split(/\t/);
@@ -40,7 +66,7 @@ close( SESNP );
 
 # Set up artifact filter
 my %filter_art;
-open( ART, "/group/cancer_informatics/tools_resources/NGS/genomes/hg19/Annotation/filter_common_artifacts.txt" );
+open( ART, $filter_common_artifacts );
 while( <ART> ) {
     chomp;
     my @a = split(/\t/);
@@ -51,7 +77,7 @@ close( ART );
 
 # Set up actionable protein mutation
 my %act_hot;
-open( ACTHOT, "/group/cancer_informatics/tools_resources/NGS/genomes/hg19/Annotation/actionable_hotspot.txt" );
+open( ACTHOT, $actionable_hotspot );
 while( <ACTHOT> ) {
     chomp;
     my ($gene, $pchg, $sg) = split(/\t/);
@@ -63,7 +89,7 @@ close( ACTHOT );
 my %act_som;
 my %act_germ;
 my %rules;
-open( ACT, "/group/cancer_informatics/tools_resources/NGS/genomes/hg19/Annotation/actionable.txt" );
+open( ACT, $actionable );
 while( <ACT> ) {
     chomp;
     my @a = split(/\t/);
@@ -91,7 +117,7 @@ close( ACT );
 # Set up Compendia Hotspot
 my %hotspotnt;
 my %hotspotprot;
-open( HOT, "/group/cancer_informatics/tools_resources/NGS/genomes/hg19/Annotation/Compendia.MS7.Hotspot.txt" );
+open( HOT, $compendia_ms7_hotspot );
 while( <HOT> ) {
     chomp;
     my @a = split(/\t/);
@@ -318,9 +344,30 @@ sub USAGE {
         If a variant is present in > [double] fraction of samples, it's deemed not a mutation.  Default: 1.0, or no filtering.
 	Use with caution.  It'll filter even if it's in COSMIC, unless if actionable. Don't use it if the sample is homogeneous.
 	Use only in heterogeneous samples.
-	
+
     -F  double
         The minimum allele frequency hotspot somatic mutations, typically lower then -f.  Default: 0.01 or half -f, whichever is less
+
+    --ruledir  dirpath
+        default is /users/kdld047/work/NGS/Wee1
+
+    --filter_common_snp  filepath
+        default is /group/cancer_informatics/tools_resources/NGS/genomes/hg19/Annotation/filter_common_snp.txt
+
+    --snpeffect_export_polymorphic  filepath
+        default is /group/cancer_informatics/tools_resources/NGS/genomes/hg19/Annotation/snpeffect_export_Polymorphic.txt
+
+    --filter_common_artifacts  filepath
+        default is /group/cancer_informatics/tools_resources/NGS/genomes/hg19/Annotation/filter_common_artifacts.txt
+
+    --actionable_hotspot  filepath
+        default is /group/cancer_informatics/tools_resources/NGS/genomes/hg19/Annotation/actionable_hotspot.txt
+
+    --actionable  filepath
+        default is /group/cancer_informatics/tools_resources/NGS/genomes/hg19/Annotation/actionable.txt
+
+    --compendia_ms7_hotspot  filepath
+        default is /group/cancer_informatics/tools_resources/NGS/genomes/hg19/Annotation/Compendia.MS7.Hotspot.txt
 
 USAGE
     exit(0);
