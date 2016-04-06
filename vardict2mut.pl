be3df18f3dac0ff07943410a933b839c61f35cf3
vladsaveliev <vladsaveliev@me.com>
2016-04-06 14:23:53 +0300

    vardict2mut.pl updated

--- vardict2mut.pl
+++ vardict2mut.pl
@@ -4,7 +4,7 @@
 use Getopt::Long qw(:config no_ignore_case);
 use strict;
 
-our ($opt_n, $opt_f, $opt_F, $opt_H, $opt_D, $opt_V, $opt_M, $opt_R, $opt_p, $opt_N, $opt_r);
+our ($opt_n, $opt_f, $opt_F, $opt_H, $opt_D, $opt_V, $opt_M, $opt_R, $opt_p, $opt_N, $opt_r, $opt_a, $opt_s, $opt_S);
 
 #my $ruledir = "/ngs/reference_data/genomes/Hsapiens/hg19/variation/rules";  # "/users/kdld047/work/NGS/Wee1";
 my $ruledir = "/users/kdld047/work/NGS/Wee1";
@@ -15,6 +15,9 @@ my $filter_common_artifacts = "$annotation_dir/filter_common_artifacts.txt";
 my $actionable_hotspot = "$annotation_dir/actionable_hotspot.txt";
 my $actionable = "$annotation_dir/actionable.txt";
 my $compendia_ms7_hotspot = "$annotation_dir/Compendia.MS7.Hotspot.txt";
+my $SPLICE = "$annotation_dir/SPLICE.txt";
+my $LASTAA = "$annotation_dir/last_critical_aa.txt";
+my $GMAF = 0.0025;
 
 GetOptions ("n=s" => \$opt_n,
             "f=f" => \$opt_f,
@@ -23,17 +26,32 @@ GetOptions ("n=s" => \$opt_n,
             "D=i" => \$opt_D,
             "V=i" => \$opt_V,
             "M"   => \$opt_M,
+            "S"   => \$opt_S,
             "N"   => \$opt_N,
             "r"   => \$opt_r,
             "R=f" => \$opt_R,
             "p=s" => \$opt_p,
+            "a=s" => \$opt_a,
+            "s=i" => \$opt_s,
+            "GMAF=f" => \$GMAF,
             "ruledir=s" => \$ruledir,
+            "annotdir=s" => \$opt_a,
             "filter_common_snp=s" => \$filter_common_snp,
             "snpeffect_export_polymorphic=s" => \$snpeffect_export_polymorphic,
             "filter_common_artifacts=s" => \$filter_common_artifacts,
             "actionable_hotspot=s" => \$actionable_hotspot,
             "actionable=s" => \$actionable,
             "compendia_ms7_hotspot=s" => \$compendia_ms7_hotspot) or USAGE();
+if ( $opt_a ) {
+    $annotation_dir = $opt_a eq "hg38" ? "/ngs/reference_data/genomes/Hsapiens/hg38/variation/cancer_informatics" : $opt_a;
+    $filter_common_snp = "$annotation_dir/filter_common_snp.txt";
+    $snpeffect_export_polymorphic = "$annotation_dir/snpeffect_export_Polymorphic.txt";
+    $filter_common_artifacts = "$annotation_dir/filter_common_artifacts.txt";
+    $actionable_hotspot = "$annotation_dir/actionable_hotspot.txt";
+    $actionable = "$annotation_dir/actionable.txt";
+    $compendia_ms7_hotspot = "$annotation_dir/Compendia.MS7.Hotspot.txt";
+    $SPLICE = "$annotation_dir/SPLICE.txt";
+}
 
 #getopts( 'HMn:f:F:D:V:R:' ) || USAGE();
 $opt_H && USAGE();
@@ -42,12 +60,32 @@ my $tp53group1 = parseMut_tp53( "$ruledir/Rules/DNE.txt" );
 my $tp53group2 = parseMut_tp53( "$ruledir/Rules/TA0-25.txt" );
 my $tp53group3 = parseMut_tp53( "$ruledir/Rules/TA25-50_SOM_10x.txt" );
 my %tp53critical_aa_pos;
-my @tp53hg19splice = qw(7574034 7574035 7576851 7576852 7576927 7576928 7577017 7577018 7577156 7577157 7577497 7577498 7577609 7577610 7578175 7578176 7578290 7578291 7578369 7578370 7578555 7578556 7579310 7579311 7579591 7579592 7579698 7579699 7579722 7579723 7579837 7579838 7574033 7576926 7577155 7577608 7578289 7578554 7579590 7579721 7576853 7577019 7577499 7578177 7578371 7579312 7579700 7579839);
-my %tp53hg19splice = map { ($_, 1); } @tp53hg19splice;
+my %splice = ();
+if ( -e $SPLICE ) {
+    open(SP, $SPLICE);
+    while( <SP> ) {
+        chomp;
+        my ($p, $g) = split(/\t/);
+        $splice{ $g }->{ $p } = 1;
+    }
+    close( SP );
+}
+
+my %lastaa = ();
+if ( -e $LASTAA ) {
+    open(AA, $LASTAA);
+    while( <AA> ) {
+        chomp;
+        my ($g, $ap, $acc) = split(/\t/);
+        $lastaa{ $g } = $ap;
+    }
+    close( AA );
+}
 
 # Set up common SNP filter
 my $MINAF = $opt_f ? $opt_f : 0.05;
 my $MAXRATE = $opt_R ? $opt_R : 1.00;
+my $MINCOMSAMPLE = $opt_s ? $opt_s : 5; # the minimum number of samples sharing the same variant
 my $ACTMINAF = $opt_F ? $opt_F : ($MINAF/2 < 0.01 ? $MINAF/2 : 0.01);
 my %filter_snp;
 open( FSNP, $filter_common_snp );
@@ -137,7 +175,7 @@ $opt_n = $opt_n ? qr/$opt_n/ : "";
 my $hdr = <>; chomp $hdr;
 my @hdrs = split(/\t/, $hdr);
 my %hdrs;
-for (my $i = 0; $i < @hdrs; $i++) {
+for(my $i = 0; $i < @hdrs; $i++) {
     $hdrs{ $hdrs[$i] } = $i;
 }
 my $passcol = $hdrs{ PASS };
@@ -155,8 +193,7 @@ if ( $opt_M ) {
     print join("\t", "SAMPLE ID", "ANALYSIS FILE LOCATION", "VARIANT-TYPE", "GENE", "SOMATIC STATUS/FUNCTIONAL IMPACT", qw(SV-PROTEIN-CHANGE SV-CDS-CHANGE SV-GENOME-POSITION SV-COVERAGE SV-PERCENT-READS CNA-COPY-NUMBER CNA-EXONS CNA-RATIO CNA-TYPE REARR-GENE1 REARR-GENE2 REARR-DESCRIPTION REARR-IN-FRAME? REARR-POS1 REARR-POS2 REARR-NUMBER-OF-READS));
     print $statuscol ? "\tStatus\n" : "\n";
 } else {
-    print "$hdr\tStatus";
-    print "\n";
+    print "$hdr\tStatus\n";
 }
 while( <> ) {
     chomp;
@@ -167,14 +204,24 @@ while( <> ) {
     $chr = "chr$chr" unless( $chr =~ /^chr/ );
     my $key = join("-", $chr, @a[2,4,5]);
     my $af = $a[$afcol];
-    my $act = isActionable( $chr, @a[2,4,5], $a[$hdrs{Gene}], $a[$hdrs{Amino_Acid_Change}], $a[$hdrs{COSMIC_AA_Change}], \@a );
+    my $act = isActionable( $chr, @a[2,4,5], $a[$hdrs{Gene}], $a[$aachgcol], $a[$cosmaachgcol], \@a );
     unless( $act ) {
         next if ( $filter_snp{ $key } );
         next if ( $snpeff_snp{ "$a[$hdrs{Gene}]-$a[$hdrs{Amino_Acid_Change}]" } );
         next if ( $filter_art{ $key } && $af < 0.20 );
+        my @gmaf = split(/,/, $a[$hdrs{GMAF}]);
+        my $flag = @gmaf ? 0 : 1;
+        foreach my $maf (@gmaf) {
+            $flag = 1 if ( $maf && $maf <= $GMAF );
+        }
+        next unless( $flag );
     }
     next if ( $opt_D && $a[$hdrs{Depth}] < $opt_D );
-    next if ( $opt_V && $a[$hdrs{VD}] < $opt_V );
+    if ( $opt_V ) {
+        if ( $a[$hdrs{VD}] < $opt_V ) {
+            next unless( $af >= 0.5 );
+        }
+    }
     my @snps = $a[3] =~ /(rs\d+)/g;
     foreach my $rs (@snps) {
         next if ( $snpeff_snp{ $rs } );
@@ -193,7 +240,7 @@ while( <> ) {
     my ($type, $fclass, $gene_coding) = @a[$typecol, $funccol, $genecodecol];
 
     # Filter low AF MSI
-    if ( abs(length($a[4])-length($a[5])) == 1 ) {  
+    if ( abs(length($a[4])-length($a[5])) == 1 ) {
         my $msi = $a[$msicol];
         if ( $msi <= 7 ) {
             next if ( $af < 0.05 );
@@ -222,7 +269,7 @@ while( <> ) {
         $status = "unknown";
     } elsif ( $a[6] =~ /FRAME_?SHIFT/i ) {
         $status = "likely";
-    } elsif ( $a[10] =~ /^[A-Z]+\d+\*$/ ) {
+    } elsif ( $aachg =~ /^[A-Z]+\d+\*$/ ) {
         $status = "likely";
     }
     if ( $type =~ /splice/i && ($type =~ /acceptor/i || $type =~ /donor/i) ) {
@@ -265,19 +312,19 @@ while( <> ) {
     } elsif ( isHotspotProt( $a[$hdrs{Gene}], $a[$hdrs{Amino_Acid_Change}] ) ) {
         $status = "likely";
     }
-    if ($act_som{ $key }) {
-        $status = "known";
-    } elsif ($act_germ{ $key }) {
-        $status = "known";
-    } elsif ( $act ) {
+    if ($act_som{ $key } || $act_germ{ $key }) {
         $status = "known";
     }
     if ( $act ) {
+        $status = "known";
         next if ( $af < $ACTMINAF );
         #next if ( $af < 0.2 && $act eq "germline" );
     } else {
         #next if ( $type =~ /^INTRON/i || $type =~ /^SYNONYMOUS_/i || $fclass eq "SILENT" || ($type =~ /splice_region_variant/ && $a[10] eq "") );
-        next if ( $type =~ /^SYNONYMOUS_/i || $fclass eq "SILENT" );
+        if ( $type =~ /^SYNONYMOUS_/i || $fclass eq "SILENT" ) {
+            next if ( $a[$classcol] eq "dbSNP" || $a[$hdrs{ID}] =~ /rs/ );
+            next if( $opt_S );
+        }
         if ( ($type =~ /^INTRON/i || ($type =~ /splice/i && $a[10] eq "")) && $status eq "unknown" ) {
             if ( $opt_N ) {
                 $status = "unknown";
@@ -297,7 +344,13 @@ while( <> ) {
         }
     }
     next if ( $a[$classcol] eq "dbSNP" && $status ne "known" );
-    if ( $opt_R && $status ne "known" && $a[$hdrs{ Pcnt_sample }] > $MAXRATE ) {
+
+    # Ignore any variants that occur after last known critical amino acid
+    if ( $aachg =~ /^[A-Z](\d+)/ ) {
+        next if ( $lastaa{ $gene } && $1 >= $lastaa{ $gene } );
+    }
+
+    if ( $opt_R && $status ne "known" && $a[$hdrs{ Pcnt_sample }] > $MAXRATE && $a[$hdrs{ N_Var }] >= $MINCOMSAMPLE ) {
         if ( $opt_r ) {
             #print "$_\t$status\n";
             print join("\t", $sample, $platform, "short-variant", $gene, $status, $a[10], $a[$hdrs{cDNA_Change}], "$chr:$a[2]", $a[$hdrs{Depth}], $af*100, "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-");
@@ -335,7 +388,7 @@ sub isActionable {
         foreach my $r ( @{ $rules{ "inframe-del" }->{ $gene } } ) {
             if ( $r->[0] eq $chr && $r->[1] <= $pos && $r->[2] >= $pos && (length($ref)-length($alt)) >= $r->[3] ) {
                 $ra->[$hdrs{Amino_Acid_Change}] = $r->[4];
-                return "somatic"; 
+                return "somatic";
             }
         }
     } elsif ( $rules{ "inframe-ins" }->{ $gene } && length($ref) < length($alt) && (length($alt)-length($ref))%3 == 0 ) {
@@ -349,7 +402,7 @@ sub isActionable {
         foreach my $r ( @{ $rules{ "indel" }->{ $gene } } ) {
             if ( $r->[0] eq $chr && $r->[1] <= $pos && $r->[2] >= $pos && abs(length($alt)-length($ref)) >= $r->[3]) {
                 $ra->[$hdrs{Amino_Acid_Change}] = $r->[4];
-                return "somatic"; 
+                return "somatic";
             }
         }
     } elsif ( $rules{ "del" }->{ $gene } && length($ref) > length($alt) ) {
@@ -395,7 +448,7 @@ sub classify_tp53 {
     $alt =~ s/\s+//g;
     $pos =~ s/\s+//g;
     $aa =~ s/\s+//g;
-    if ( $tp53hg19splice{ $pos } && length($ref) == 1 && length($alt) == 1 ) {
+    if ( $splice{ TP53 }->{ $pos } && length($ref) == 1 && length($alt) == 1 ) {
         return "Group 6";
     }
     $aa =~ s/^p.//;
@@ -431,10 +484,13 @@ sub USAGE {
     print STDERR <<USAGE;
     Usage: $0 [-n reg_name] input
     The program will filter the VarDict output after vcf2txt.pl to candidate interpretable mutations, somatic or germline.
-     
+
     Options:
     -H  Print this usage
     -M  Output in FM's format
+    -S        Skip the silent mutation that are "unknown" in Status.  By default: all silent "Novel" and "COSMIC" only mutations will be kept.
+        Silent mutation with entries in both COSMIC and dbSNP will be filtered.
+
     -N  If set, keep all intronic and UTR in the output, but will be set as "unknown".
     -D  int
         The minimum total depth
@@ -449,8 +505,13 @@ sub USAGE {
 
     -R  double
         If a variant is present in > [double] fraction of samples, it's deemed not a mutation.  Default: 1.0, or no filtering.
-        Use with caution.  It'll filter even if it's in COSMIC, unless if actionable. Don't use it if the sample is homogeneous.
-        Use only in heterogeneous samples.
+        Use with caution.  It'll filter even if it's in COSMIC, unless it's on actionable list. Don't use it if the cohort is homogeneous.
+        Use only for heterogeneous cohorts.  Used together with -s option.
+
+    -s  integer
+        The minimum number of samples that sharing the same variant that are not on the known list to be filtered out.  Used together
+        with -R option.  A variant has to satisfied both conditions, AND not on the knon list, to be considered too common to
+        be functional and filtered out.  Use only for heterogeneous cohorts. Default: 5
 
     -r  If set, keep only those variants satisfying -R option.  The option is meant to find what are re-occuring variants or
         artifacts.
@@ -461,8 +522,20 @@ sub USAGE {
     -p  string
         The platform, such as WXS, WGS, RNA-Seq, VALIDATION, etc.  No Default.  Used when output is in FM's format (-M option)
 
+    -a  dirpath
+        Directory path for containing annotation files.
+        Default is /group/cancer_informatics/tools_resources/NGS/genomes/hg19/Annotation
+        For hg38, use /group/cancer_informatics/tools_resources/NGS/genomes/hg38/Annotation
+        If dirpath is "hg38", automatically set it to /group/cancer_informatics/tools_resources/NGS/genomes/hg38/Annotation
+
+    --GMAF double
+        When the GMAF is greater than specified, it's considered common SNP and filtered out.  Default: 0.0025 (0.25%)
+
     --ruledir  dirpath
         default is /users/kdld047/work/NGS/Wee1
+
+    --annotdir  dirpath
+        default is /group/cancer_informatics/tools_resources/NGS/genomes/hg19/Annotation
 
     --filter_common_snp  filepath
         default is /group/cancer_informatics/tools_resources/NGS/genomes/hg19/Annotation/filter_common_snp.txt
